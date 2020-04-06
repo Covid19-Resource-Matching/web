dc30f1d7919eb71e25add2f34e8c9f59a2832955
whatthestack <sonhelpdesk@gmail.com>
2020-04-06 00:09:05 -0700

    Added signup, reset password, profile update, and deletion.

--- backend/covid/urls.py
+++ backend/covid/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .views import SignUpView, ProfileView
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -12,4 +13,7 @@ urlpatterns = [
     ),
     path("supplier", views.supplier, name="supplier"),
     path("update_supplies", views.update_supplies, name="update_supplies"),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('userprofile/<int:pk>/', ProfileView.as_view(), name='userprofile'),
+    path('deleteuser', views.deleteuser, name='deleteuser'),
 ]
