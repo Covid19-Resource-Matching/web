c75f7028e53bfeb45733a09e1f351874674ca334
rock <yulan.lin.2014@gmail.com>
2020-04-12 23:51:34 -0700

    ran black to format files

--- backend/covid/migrations/0003_auto_20200322_2027.py
+++ backend/covid/migrations/0003_auto_20200322_2027.py
@@ -6,16 +6,10 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('covid', '0002_auto_20200316_2056'),
+        ("covid", "0002_auto_20200316_2056"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='request',
-            name='timestamp',
-        ),
-        migrations.RemoveField(
-            model_name='supply',
-            name='timestamp',
-        ),
+        migrations.RemoveField(model_name="request", name="timestamp",),
+        migrations.RemoveField(model_name="supply", name="timestamp",),
     ]
