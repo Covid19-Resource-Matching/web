d18d5681fae388ad9ed0ada16570eddcda4f9d80
Sarah Foster <sarahhhhfoster@gmail.com>
2020-05-14 22:10:40 -0700

    Update urls.py
    Removing an unnecessary include that's already covered within `covid.urls`, under the 'deleteuser' path.

--- backend/covid_resource_matcher/urls.py
+++ backend/covid_resource_matcher/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("accounts/", include("covid.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("covid/", include("covid.urls")),
     path("admin/", admin.site.urls),
